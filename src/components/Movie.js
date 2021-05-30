baaeb1c1f7fa70be7b6af127f7419284e070cfce
irah rosete <irah.rosete@outlook.com>
2021-05-31 00:02:23 +1000

    add react-icons fatimes

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,12 @@
+import { FaTimes } from 'react-icons/fa'
+
 const Movie = ({ movie }) => {
   return (
     <div className="movie">
-      <h3>{movie.title}</h3>
+      <h3>
+        {movie.title}
+        <FaTimes />
+      </h3>
       <p>{movie.category}</p>
     </div>
   )
